72a8aeaa7220e3d9d8aef542dd5bb2f1648d6200
Lenovo <Lenovo@LAPTOP-NSMJD0QF>
2018-11-30 11:29:10 +0600

    Добавьте файлы проекта.

---
+++ CarDBSQLApplication/Pages/AddNewCarPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using CarDBSQLApplication_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CarDBSQLApplication.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewCarPage.xaml
+    /// </summary>
+    public partial class AddNewCarPage : Page
+    {
+        public AddNewCarPage()
+        {
+            InitializeComponent();
+
+            List<TablesManufacturer> lct = new List<TablesManufacturer>();
+            List<TablesModel> modelLct = new List<TablesModel>();
+            List<TablesSNPrefix> prefixLct = new List<TablesSNPrefix>();
+
+            lct = AddNewCarPage.GetManufactList();
+            ManufactList.ItemsSource = lct;
+
+            modelLct = AddNewCarPage.GetModelList();
+            ModelList.ItemsSource = modelLct;
+
+            prefixLct = AddNewCarPage.GetPrefixList();
+            PrefixList.ItemsSource = prefixLct;
+        }
+
+        public static List<TablesManufacturer> GetManufactList()
+        {
+            EntityModel db = new EntityModel();
+
+            return db.TablesManufacturer.Select(p => p).ToList<TablesManufacturer>();
+        }
+
+        public static List<TablesModel> GetModelList()
+        {
+            EntityModel db = new EntityModel();
+
+            return db.TablesModel.Select(p => p).ToList<TablesModel>();
+        }
+
+        public static List<TablesSNPrefix> GetPrefixList()
+        {
+            EntityModel db = new EntityModel();
+
+            return db.TablesSNPrefix.Select(p => p).ToList<TablesSNPrefix>();
+        }
+
+        public void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Close_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mf.Source = new Uri("Pages/MainPage.xaml", UriKind.RelativeOrAbsolute);
+            MainPage mp = new MainPage();
+            MainWindow.mf.NavigationService.Navigate(mp);
+        }
+    }
+}
---
+++ CarDBSQLApplication/Pages/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using CarDBSQLApplication_DAL;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data.Entity;
+using System.Windows.Documents;
+
+namespace CarDBSQLApplication.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        EntityModel db;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            try
+            {
+                db = new EntityModel();
+                var EquipList = from d in db.newEquipment
+                                join s in db.TablesManufacturer on d.intManufacturerID equals s.intManufacturerID
+                                join t in db.TablesModel on d.intModelID equals t.intModelID
+                                join v in db.TrackMeter on d.intEquipmentID equals v.intEquipmentID
+                                select new { d.intGarageRoom, s.strName, t.strModelName, d.strManufYear, d.strSerialNo, v.intHoursHoursOperation, v.intMeterReading };
+               
+                equipGrid.ItemsSource = EquipList.ToList(); // устанавливаем привязку к кэшу
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mf.Source = new Uri("Pages/AddNewCarPage.xaml", UriKind.RelativeOrAbsolute);
+            AddNewCarPage ap = new AddNewCarPage();
+            MainWindow.mf.NavigationService.Navigate(ap);
+        }
+
+        public void onHyperlinkClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mf.Source = new Uri("Pages/DetailsPage.xaml", UriKind.RelativeOrAbsolute);
+            DetailsPage dp = new DetailsPage();
+            MainWindow.mf.NavigationService.Navigate(dp);
+
+        }
+    }
+}
